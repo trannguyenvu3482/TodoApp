ffabf04a47781af0d886497e67ade7f5aa4ca4e0
trannguyenvu3482 <87817412+trannguyenvu3482@users.noreply.github.com>
2022-03-06 17:24:43 +0700

    Update App.js

--- component/App.js
+++ component/App.js
@@ -1,6 +1,6 @@
 import html from '../core.js';
 import { connect } from '../store.js';
-import Header from '../component/header.js';
+import Header from '../component/Header.js';
 import TodoList from '../component/TodoList.js';
 import Footer from '../component/Footer.js';
 
